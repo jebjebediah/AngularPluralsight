0b80a8021bd6cc8e72f5501f572d033ac32fe208
Jackson Bishop <jackson.bishop@lcs.com>
2021-10-19 17:39:44 -0400

    After Section 6

--- src/app/home/app.component.ts
+++ src/app/home/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  template: `<h1>{{pageTitle}}</h1>`
+  template: `
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <pm-products></pm-products>
+  </div>`
 })
 export class AppComponent {
   pageTitle: string = "Product Management Console"
